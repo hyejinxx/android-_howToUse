c69cb410f3cc88f50c48513ecc19488385d7e6d3
hyejinxx <100978925+hyejinxx@users.noreply.github.com>
2022-03-05 11:43:05 +0900

    Update and rename GoogleLogin.kt to LoginCheck.kt

--- LoginCheck.kt
+++ LoginCheck.kt
@@ -5,6 +5,7 @@ import com.google.firebase.auth.FirebaseUser
 
 
 auth = FirebaseAuth.getInstance()
+val pageMove = Intent(this, SignActivity::class.java)
 
 
 Handler(Looper.getMainLooper()).postDelayed({
@@ -12,10 +13,11 @@ Handler(Looper.getMainLooper()).postDelayed({
     if(account!==null){ // 이미 로그인 되어있을시 바로 메인 액티비티로 이동
         toMainActivity(auth.currentUser)
     }
-    else{startActivity(pageMove)}
+    else{startActivity(pageMove)} // 회원가입 페이지로 이동
 }, 2500L)
 
 }
+//로그인 되어있을 시 스타트 페이지 거쳐서 메인으로 이동
 fun toMainActivity(user: FirebaseUser?) {
     if (user != null) {
         startActivity(Intent(this, MainActivity::class.java))
